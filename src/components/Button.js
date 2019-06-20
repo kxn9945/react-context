dee82644dbb4e734e61716c549aa183f9306503d
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-06-20 12:35:29 -0400

    react-context initial

---
+++ src/components/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext';
+
+
+class Button extends React.Component{
+  //add property to the class need to be contextType exactly
+  //using this.context approach
+  //static contextType = LanguageContext;
+
+  //OR USE CONSUMER APPROACH
+  // TO pull multiple different context object
+        // <LanguageContext.Consumer>
+        //   {(value) => this.renderSubmit(value)}
+        // </LanguageContext.Consumer>
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'ส่ง';
+  }
+
+  render() {
+    //const text = this.context === 'english' ? 'Submit' : 'ส่ง';
+
+    return (
+      <ColorContext.Consumer>
+        {(color) =>
+          <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {(value) => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+          </button>
+        }
+
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
